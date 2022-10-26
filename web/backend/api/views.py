a682a310abc6c8ffb137dad20846424c552b5b07
gamingflexer <osurve20ece@student.mes.ac.in>
2022-10-26 14:02:02 +0530

    camera video feed added

--- web/backend/api/views.py
+++ web/backend/api/views.py
@@ -3,3 +3,17 @@ from flask_restful import Resource
 class helloworld(Resource):
     def get(self):
         return "Hello World!"
+    
+'''
+    crime_ref = db.collection(u'crimes')
+    docs = crime_ref.stream()
+
+    for doc in docs:
+        data1 = data.update({doc.id : doc.to_dict()})
+        
+    db.collection("cluster_test").add(test)
+    
+    "lastUpdated":firestore.SERVER_TIMESTAMP
+    
+    delete_collection(cluster_ref,3)
+'''
